c9ecfc3b0d2b0d18aeb531f18f0c26935cc9e22c
Jazcash <jaspercashmore@gmail.com>
2021-06-06 19:24:59 +0100

    center page

--- pages/balance-changes/index.vue
+++ pages/balance-changes/index.vue
@@ -87,6 +87,7 @@ export default class Page extends Vue {
     display: flex;
     flex-direction: column;
     justify-content: center;
+    align-items: center;
 }
 .balance-changes {
     max-width: 800px;
